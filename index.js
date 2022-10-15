b206b199fdd43232dbe15c32cf311d93e4ec682c
medilies <medilies@gmail.com>
2022-10-15 19:23:54 +0100

    subscibe to a JSON of topics

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const config = require("./config");
 
+const fs = require("fs");
 const mqtt = require("mqtt");
 
 const mqttUtils = require("./mqtt/utils");
+
+const subTopics = JSON.parse(fs.readFileSync("./sub_topics.json"));
 
 const client = mqtt.connect(mqttUtils.formatConnectionOpts(config.mqtt));
 
 console.log(client.options);
 
-client.on("connect", function () {
-    client.subscribe("presence", function (err) {
-        if (!err) {
-            client.publish("presence", "Hello mqtt");
-        }
+client.on("connect", () => {
+    console.log(Date() + ": CONNECTED!");
+
+    client.subscribe(subTopics, (err, grant) => {
+        // if (err) throw err;
+        if (err) console.warn(Date() + ": SUBSCRIBE ERROR:", err);
+        console.log(Date() + ": SUBSCRIBED TO :\n", grant);
     });
 });
 
 client.on("message", function (topic, message) {
     // message is Buffer
     console.log(message.toString());
+    // ...
     client.end();
 });
