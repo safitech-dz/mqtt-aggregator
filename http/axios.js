839f10870a83a4ed34d37bac5e4d79f5a761079e
medilies <medilies@gmail.com>
2022-11-05 17:22:58 +0100

    authenticate axios with bearer token

--- http/axios.js
+++ http/axios.js
@@ -1,5 +1,20 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosBaseUrl = axios.create({
     baseURL: "http://localhost:8000/api",
+});
+
+axiosBaseUrl.defaults.withCredentials = true;
+
+const res = await axiosBaseUrl.post("/login", {
+    email: "test@example.com",
+    password: "password",
+});
+
+const token = res.data.token;
+
+export default axiosBaseUrl.create({
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
 });
